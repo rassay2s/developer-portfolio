8f2e8fde38c252ec1592b87dcbdc5b1f97728aa6
Rami Assayed <zrami.as.591@gmail.com>
2024-12-24 05:03:08 +0100

    Home screen done

---
+++ script.js
@@ -0,0 +1,9 @@
+// close icon
+function colseNav(){
+    let navMenu = document.getElementById('navmenu');
+    navMenu.style.right = "-300px";
+}
+function openNav(){
+    let navMenu = document.getElementById('navmenu');
+    navMenu.style.right = "0";
+}
